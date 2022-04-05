bb9fd4253892c9f8706c38c624e63e4142668cca
DESKTOP-5EMPGQL\Esmanios <beoms42@gmail.com>
2022-04-05 15:00:52 +0900

    +procedure 3X 근데 REPORT에 LIST 해시맵에 데이터값 어케넣는지 모름 안들어감

--- sakila/src/main/java/dao/FilmDao.java
+++ sakila/src/main/java/dao/FilmDao.java
@@ -43,17 +43,79 @@ public class FilmDao {
 		map.put("count", count);
 		return map;
 	}
-	public static void main(String[] args) {
-		FilmDao fd = new FilmDao();
-		int filmId = 7;
-		int storeId = 2;
-		Map<String, Object> map = fd.filmInStockCall(filmId, storeId);
-		List<Integer> list = (List<Integer>)map.get("list");
-		int count = (Integer)map.get("count");
-		
-		System.out.println(filmId + "번 영화는 "+ storeId +"번 가게에 "+count+"개 남음");
-		for(int id : list) {
-			System.out.println(id);
+	
+	public Map<String, Object> filmNotInStockCall(int filmId, int storeId) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		Connection conn = null;
+		// PreparedStatement : 쿼리를 실행
+		// CallableStatement : 프로시저를 실행 
+		CallableStatement stmt = null;
+		ResultSet rs = null;
+		// select inventory_id .... 
+		List<Integer> list = new ArrayList<>();
+		// select count(inventroy_id) ....
+		Integer count = 0;
+		conn = DBUtil.getConnection();
+		try {
+			stmt = conn.prepareCall("{call film_not_in_stock(?, ?, ?)}");
+			stmt.setInt(1, filmId);
+			stmt.setInt(2, storeId);
+			stmt.registerOutParameter(3, Types.INTEGER);
+			rs = stmt.executeQuery();
+			while(rs.next()) {
+				list.add(rs.getInt(1)); // rs.getInt("inventory_id")
+			}
+			count = stmt.getInt(3); // 프로시저 3번째 out변수 값
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+		map.put("list", list);
+		map.put("count", count);
+		return map;
+	}
+	
+	public Map<String, Object> rewordsReport(int min_monthly_purchases, int min_dollar_amount_purchased) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		Connection conn = null;
+		// PreparedStatement : 쿼리를 실행
+		// CallableStatement : 프로시저를 실행 
+		CallableStatement stmt = null;
+		ResultSet rs = null;
+		// 제일 큰 맵 밑 어레이리스트 밑 해시맵
+		HashMap<String, Object> maps = null;
+		//위의 해시맵을 포함할 어레이리스트 
+		//계층도 :  Map<String, Object> 밑 ArrayList<HashMap<String, Object>> 밑 HashMap<String, Object> maps
+		ArrayList<HashMap<String, Object>> list = null;
+		
+		// select count(inventroy_id) ....
+		Integer count = 0;
+		conn = DBUtil.getConnection();
+		try {
+			stmt = conn.prepareCall("{call rewards_report(?, ?, ?)}");
+			stmt.setInt(1, min_monthly_purchases);
+			stmt.setInt(2, min_dollar_amount_purchased);
+			stmt.registerOutParameter(3, Types.JAVA_OBJECT);
+			rs = stmt.executeQuery();
+			while(rs.next()) {
+				maps = new HashMap<String, Object>();
+				maps.put("customerId", rs.getInt("customer_id"));
+				maps.put("storeId", rs.getInt("store_id"));
+				maps.put("firstName", rs.getString("first_name"));
+				maps.put("email", rs.getString("email"));
+				maps.put("addressId", rs.getString("address_id"));
+				maps.put("active", rs.getString("active"));
+				maps.put("createDate", rs.getString("create_date"));
+				maps.put("lastUpdate", rs.getString("last_update"));
+				
+				list.add(maps);
+			}
+			count = stmt.getInt(3); // 프로시저 3번째 out변수 값 : count_rewardees
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println(list);
+		map.put("list", list);
+		map.put("count", count);
+		return map;
 	}
 }
--- sakila/src/main/webapp/inc/leftMenu.jsp
+++ sakila/src/main/webapp/inc/leftMenu.jsp
@@ -1,6 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=EUC-KR"
     pageEncoding="EUC-KR"%>
+    	   
 		<ol>
+		View List
 			<li><a href="<%=request.getContextPath() %>/index.jsp">INDEX</a></li>
 			<li><a href="<%=request.getContextPath() %>/storeList.jsp">Store List</a></li>
 			<li><a href="<%=request.getContextPath() %>/staffList.jsp">Staff List</a></li>
@@ -10,4 +12,11 @@
             <li><a href="<%=request.getContextPath()%>/niceButSlowerFilmList(view).jsp">NicerButSlowerFilm List(view)</a></li>
             <li><a href="<%=request.getContextPath()%>/salesByStoreList(view).jsp">SalesByFilmCategory List(View)</a></li>
             <li><a href="<%=request.getContextPath()%>/salesByStoreList(view).jsp">SalesByStore List(View)</a></li>
+		</ol>
+		
+		<ol>
+		Procedure List
+			<li><a href="<%=request.getContextPath() %>/procedureFilmInStock.jsp">procedureFilmInStock</a></li>
+			<li><a href="<%=request.getContextPath() %>/procedureFilmNotInStock.jsp">procedureFilmNotInStock</a></li>
+			<li><a href="<%=request.getContextPath() %>/procedureReport.jsp">procedureReport</a></li>
 		</ol>
